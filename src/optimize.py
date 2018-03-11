06dfd65efce8d8b077862a97352e1cc6b11e2f52
Julian <julianbrendl@gmail.com>
2018-03-11 12:28:59 +0100

    git added stuff

--- src/optimize.py
+++ src/optimize.py
@@ -18,18 +18,18 @@ logger = logger.get_logger()
 
 
 def pre_process_loop(args, pre_processor):
-    #loop_interval = int(args.num_bins / args.num_workers)
-    #loop_offset = (args.bin_id - 1) * loop_interval
-    #start_bin = 1 + loop_offset
-    #end_bin = loop_interval + loop_offset
+    loop_interval = int(args.num_bins / args.num_workers)
+    loop_offset = (args.bin_id - 1) * loop_interval
+    start_bin = 1 + loop_offset
+    end_bin = loop_interval + loop_offset
 
-    #for i in range(start_bin, end_bin + 1):
-    #    pre_processor.pre_process(i, args.num_bins)
+    for i in range(start_bin, end_bin + 1):
+       pre_processor.pre_process(i + 1, args.num_bins)
 
-    if args.bin_id == 1:
-        pre_processor.pre_process(10, args.num_bins)
-    elif args.bin_id == 9:
-        pre_processor.pre_process(13, args.num_bins)
+    # if args.bin_id == 1:
+    #     pre_processor.pre_process(10, args.num_bins)
+    # elif args.bin_id == 9:
+    #     pre_processor.pre_process(13, args.num_bins)
 
 
 def handle_model_type(args):
